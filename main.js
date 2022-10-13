667e7b4115a555c33309fda2f4f8521fe839292f
spandemic <anloeng@ucsc.edu>
2022-10-13 09:18:29 -0700

    working game

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ description = `
 characters = [
 `
 gggggg
-gggggg
-gggggg
-gggggg
-gggggg
+g    g
+g gg g
+g gg g
+g    g
 gggggg
 `,`
 y yy y
@@ -17,6 +17,20 @@ y yy y
   yy  
 yy  yy
 y    y
+`,`
+gggggg
+gggggg
+gggggg
+gggggg
+gggggg
+gggggg
+`,`
+rrrrrr
+rrrrrr
+rrrrrr
+rrrrrr
+rrrrrr
+rrrrrr
 `
 ];
 
@@ -24,7 +38,9 @@ const G = {
     WIDTH: 200,
     HEIGHT: 200,
 
-    ENEMY_BASE_SPEED: 60
+    ENEMY_BASE_SPEED: 70,
+
+    GAP: 6
 };
 
 options = {
@@ -36,51 +52,122 @@ options = {
 };
 
 /**
- * @type {{pos: Vector, vel: Vector, radius: number}}
+ * @type {{pos: Vector}}
  */
 let player;
 
 /**
- * @type {{pos: Vector, type: number, vel: number, speed: number}[]}
+ * @type {{pos: Vector, type: number}[]}
  */
 let cells;
 let nextCellTicks;
 
 /**
- * @type {{pos: Vector, radius: number}[]}
+ * @type {{pos: Vector, type: number}[]}
  */
-let explosions;
+let wallBlock;
+let wallFacing;
+
 let waveDiff;
+let playerHealth;
 
 function update() {
   if (!ticks) {
     player = {
-      pos: vec(G.WIDTH / 2, G.HEIGHT / 2),
-      vel: vec(),
-      radius: 0
+      pos: vec(G.WIDTH / 2, G.HEIGHT / 2)
     };
     cells = [];
     nextCellTicks = 0;
-    explosions = [];
+    wallBlock = [];
+
+    wallFacing = 1;
+
     waveDiff = 1;
+    playerHealth = 3;
+
+    // times(40, (i) => {
+    //   const pos = vec(G.WIDTH/2, G.HEIGHT/2).addWithAngle(i / (PI * 2), 30);
+    //   const type = i < 10 ? 1 : 0;
+    //   wallBlock.push({ pos, type });
+    // })
+
+    // fuck this shit LMAO
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 3 * G.GAP, G.HEIGHT/2 - 3 * G.GAP), type: 1});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 2 * G.GAP, G.HEIGHT/2 - 3 * G.GAP), type: 1});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 1 * G.GAP, G.HEIGHT/2 - 3 * G.GAP), type: 1});
+    wallBlock.push({ pos: vec(G.WIDTH/2, G.HEIGHT/2 - 3 * G.GAP), type: 1});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 1 * G.GAP, G.HEIGHT/2 - 3 * G.GAP), type: 1});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 2 * G.GAP, G.HEIGHT/2 - 3 * G.GAP), type: 1});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 3 * G.GAP, G.HEIGHT/2 - 3 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 3 * G.GAP, G.HEIGHT/2 - 2 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 3 * G.GAP, G.HEIGHT/2 - 1 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 3 * G.GAP, G.HEIGHT/2), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 3 * G.GAP, G.HEIGHT/2 + 1 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 3 * G.GAP, G.HEIGHT/2 + 2 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 3 * G.GAP, G.HEIGHT/2 + 3 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 2 * G.GAP, G.HEIGHT/2 + 3 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 + 1 * G.GAP, G.HEIGHT/2 + 3 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2, G.HEIGHT/2 + 3 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 1 * G.GAP, G.HEIGHT/2 + 3 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 2 * G.GAP, G.HEIGHT/2 + 3 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 3 * G.GAP, G.HEIGHT/2 + 3 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 3 * G.GAP, G.HEIGHT/2 + 2 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 3 * G.GAP, G.HEIGHT/2 + 1 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 3 * G.GAP, G.HEIGHT/2), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 3 * G.GAP, G.HEIGHT/2 - 1 * G.GAP), type: 0});
+    wallBlock.push({ pos: vec(G.WIDTH/2 - 3 * G.GAP, G.HEIGHT/2 - 2 * G.GAP), type: 0});
   }
 
-  color("black");
+  if (playerHealth === 3) {
+    color("green");
+  } else if (playerHealth === 2){
+    color("yellow");
+  } else if (playerHealth === 1) {
+    color("red");
+  } else {
+    color("light_red")
+    end();
+  }
   char("a", player.pos.x, player.pos.y);
   
+  
+
+  if(input.isJustPressed) {
+    let counter = 0;
+    for(i = 0; i < wallBlock.length; i++) {
+      if(wallBlock[i].type == 1 && counter < 6) {
+        let index = i;
+        let wallType = i + 6 > 23 ? (index - 24) + 6 : index + 6;
+        wallBlock[wallType].type = 1;
+        wallBlock[i].type = 0;
+
+        counter++;
+      }
+    }
+    counter = 0;
+  }
+
   nextCellTicks--;
   if (nextCellTicks < 0) {
-    let i = floor(rnd(5, 10) * difficulty);
+    let i = floor(rnd(3, 9) * difficulty);
     times(i, () => {
-      const pos = vec(G.WIDTH / 2, G.HEIGHT / 2).addWithAngle(rnd(PI * 2), rnd(40, 150));
+      const pos = vec(G.WIDTH / 2, G.HEIGHT / 2).addWithAngle(rnd(PI * 2), rnd(200, 600));
       const type = (rnd(10, 50) < 40 ? 1 : 0);
-      const vel = rnd(1, sqrt(difficulty)) * 0.01;
-      const speed = sqrt(difficulty) * 0.4;
-      cells.push({ pos, type, vel, speed });
+      cells.push({ pos, type });
     });
-    nextCellTicks = rnd(50, 80);
+    nextCellTicks = rnd(80, 120);
+    waveDiff += 1;
+    if (G.ENEMY_BASE_SPEED > 40 && waveDiff % 11 === 0) {
+      G.ENEMY_BASE_SPEED -= 5;
+      console.log("speedup");
+    }
   }
-  
+
+  remove(wallBlock, (w) => {
+    color(w.type === 1 ? "cyan" : "red");
+    w.type === 1 ? char("c", w.pos) : char("d", w.pos);
+  });
+
   remove(cells, (c) => {
     
     // moves cells towards player/center of screen
@@ -90,9 +177,31 @@ function update() {
     yAngle < 0 ? c.pos.y += -(yAngle / G.ENEMY_BASE_SPEED) : c.pos.y -= (yAngle / G.ENEMY_BASE_SPEED);
     
     color(c.type == 1 ? "cyan" : "red");
-    char("b", c.pos);
+    const isCollidingWithWall = char("b", c.pos).isColliding.char.d;
+    const isCollidingWithAbsorber = char("b", c.pos).isColliding.char.c;
+    const isCollidingWithPlayer = char("b", c.pos).isColliding.char.a;
 
-    // return()
-  })
+    if(isCollidingWithPlayer) {
+      if (c.type === 1) {
+        addScore(10 + waveDiff, c.pos);
+        play("powerUp");
+        color("green");
+        particle(c.pos);
+      } else {
+        color("red");
+        particle(c.pos);
+        play("hit");
+        playerHealth--
+      }
+    }
 
+    if(isCollidingWithAbsorber && input.isJustPressed) {
+      color(c.type == 1 ? "cyan" : "red");
+      particle(c.pos);
+      play("coin");
+      addScore(10, c.pos);
+    }
+
+    return(isCollidingWithWall || isCollidingWithPlayer);
+  });
 }
